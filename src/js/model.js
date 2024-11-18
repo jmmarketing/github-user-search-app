4ec1ce7c8d33c4009b2b1b04e0cb3dd491729102
jmmarketing <jeff@jeffreymclean.com>
2024-11-18 13:32:26 -0800

    Created component SCSS files and JS view files. Will have 3 (infobar, searchbar, resultcard).

---
+++ src/js/model.js
@@ -0,0 +1,2 @@
+//[GitHub users API](https://docs.github.com/en/rest/reference/users#get-a-user)
+//The GitHub users API endpoint is `https://api.github.com/users/:username`. So, if you wanted to search for the Octocat profile, you'd be able to make a request to `https://api.github.com/users/octocat`.
