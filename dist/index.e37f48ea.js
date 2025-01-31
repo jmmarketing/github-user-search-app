dd9a84d97af3f5936125d5ff9212c356593d11ad
jmmarketing <jeff@jeffreymclean.com>
2025-01-31 14:30:03 -0800

    Resuming working on the light/dark toggle. Per last log working on element updates. Added a state variable in controller. Will use to determine which element should be used in the nav__toggle element.

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -586,12 +586,27 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"aenu9":[function(require,module,exports) {
 const modeToggle = document.querySelector("#mode-toggle");
 const bodyElement = document.querySelector("body");
+const state = {
+    mode: "light"
+};
 modeToggle.addEventListener("click", function() {
     console.log("Light/Dark Mode Toggle!");
     console.log(bodyElement);
-    if (!bodyElement.dataset.theme) bodyElement.dataset.theme = "dark";
-    else delete bodyElement.dataset.theme;
+    //
+    if (!bodyElement.dataset.theme) {
+        bodyElement.dataset.theme = "dark";
+        modeToggle.dataset.theme = "dark";
+        state.mode = "dark";
+    } else {
+        delete bodyElement.dataset.theme;
+        delete modeToggle.dataset.theme;
+        state.mode = "light";
+    }
+    updateModeToggleElement();
 });
+function updateModeToggleElement() {
+    console.log(state.mode);
+}
 
 },{}]},["hycaY","aenu9"], "aenu9", "parcelRequire1189")
 
